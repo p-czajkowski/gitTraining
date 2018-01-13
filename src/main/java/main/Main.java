c967f1a30f0bc047fc3ffbddc084f861b29bee42
Piotr <p-czajkowski@outlook.com>
2018-01-13 12:17:55 +0100

    task2 message next

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
+        System.out.println("hello task2");
+
+
         System.out.println("New bleble");
 
         System.out.println("Hello from task2");
